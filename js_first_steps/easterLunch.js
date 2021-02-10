8539c8eea0bb5cc4b5d165d930cf10f2c77b1693
Nina Stoyanova <nina.stoqnova91@gmail.com>
2021-02-10 17:38:15 +0200

    exersices

--- js_first_steps/easterLunch.js
+++ js_first_steps/easterLunch.js
@@ -1,11 +1,11 @@
 
 
-function easterLunch (easterBread, packOfEggs, cookies) {
+function easterLunch (input) {
     
-    let easterBreadPrice = Number(easterBread) * 3.2; //9.600000000000001
-    let packOfEggsPrice = Number(packOfEggs) * 4.35;//8.7
-    let cookiesPrice = Number(cookies) * 5.4; //16.200000000000003
-    let eggsPaint = Number(packOfEggs) * 12 * 0.15;//3.5999999999999996
+    let easterBreadPrice = Number(input[0]) * 3.2; //9.600000000000001
+    let packOfEggsPrice = Number(input[1]) * 4.35;//8.7
+    let cookiesPrice = Number(input[2]) * 5.4; //16.200000000000003
+    let eggsPaint = Number(input[1]) * 12 * 0.15;//3.5999999999999996
 
     let summery = easterBreadPrice + packOfEggsPrice + cookiesPrice + eggsPaint;// 38.1
     let finalSummery = summery.toFixed(2);
@@ -15,4 +15,4 @@ function easterLunch (easterBread, packOfEggs, cookies) {
 
 }
 
-easterLunch("3", "2", "3");
+easterLunch(["3", "2", "3"]);//easterBread, packOfEggs, cookies
