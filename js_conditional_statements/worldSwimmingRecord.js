d3fc5a188fc53325a91b8c405ab4006482de5584
Nina Stoyanova <nina.stoqnova91@gmail.com>
2021-02-18 12:41:53 +0200

    excersises

--- js_conditional_statements/worldSwimmingRecord.js
+++ js_conditional_statements/worldSwimmingRecord.js
@@ -3,27 +3,30 @@ function worldSwimmingRecord(input) {
     let distanceInMeters = Number(input[1]);//1500m, 3017m
     let timeForOneMeterInSecods = Number(input[2]);//20s, 5.03s
 
-    let SwamTimeInSeconds = distanceInMeters * timeForOneMeterInSecods;//30000s, 15175.51s
+    let swamTimeInSeconds = distanceInMeters * timeForOneMeterInSecods;//30000s, 15175.51s
     
     let resistance = distanceInMeters / 15;//100, 201.133333333
 
     let resistanceFinal = Math.floor(resistance) * 12.5;//1250s, 2512.50
-
     
-    let sumTime = SwamTimeInSeconds + resistanceFinal;//31250, 17688.0100000
+    let sumTime = swamTimeInSeconds + resistanceFinal;//31250, 17688.0100000
 
-    let difference = 0;
-    if (recordInSeconds < sumTime) {
-        difference = sumTime - recordInSeconds;
-        console.log(`No, he failed! He was ${difference.toFixed(2)} seconds slower.`);
+    let difference = recordInSeconds - sumTime;//+ -//17688.01
+
+    if (difference > 0) {
+       
+        console.log(`Yes, he succeeded! The new world record is ${sumTime.toFixed(2)} seconds.`);
     } else {
-        difference = sumTime + 0;
-        console.log(`Yes, he succeeded! The new world record is ${difference.toFixed(2)} seconds.`);
+        difference = Math.abs(difference);
+        console.log(`No, he failed! He was ${difference.toFixed(2)} seconds slower.`);
     }
         
-
-
 }
 
 //worldSwimmingRecord(["10464","1500","20"]);
+//worldSwimmingRecord(["55555.67","3017","5.03"]);
 worldSwimmingRecord(["55555.67","3017","5.03"]);
+
+
+
+
